540f7a07399e6314b3010496a62d083b048d9af5
Atyrkyl <atyrkyl.st@mail.ru>
2021-09-04 17:40:53 +0300

    gg

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        final String hello = "Привет,";
+        String world = "мир!";
+
+        System.out.println(hello + world);
+    }
+}
